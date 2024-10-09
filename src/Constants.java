008e1f50715aad4250561e64af7f63fedb9d8a3b
shaiksharif <shaikmastansharif116@gmail.com>
2024-10-09 07:32:06 +0530

    my files lab1

---
+++ src/Constants.java
@@ -0,0 +1,9 @@
+public class Constants {
+    public static String  firstName =" shaik";
+    public static  String LastName ="sharif";
+
+    public static void main(String[] args) {
+        System.out.println("firstname " + firstName);
+        System.out.println("lastName "+ LastName);
+    }
+}
