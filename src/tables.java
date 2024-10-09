8c164fc67dc29b406966b0709da0b26e77d465aa
shaiksharif <shaikmastansharif116@gmail.com>
2024-10-09 07:25:37 +0530

    my files lab1

---
+++ src/tables.java
@@ -0,0 +1,16 @@
+public class tables {
+
+    public static void main(String[] args) {
+        int  a=13;
+        System.out.printf("%d x 1=%d%n", a , a *2 );
+        System.out.printf("%d x 1=%d%n", a , a *3 );
+        System.out.printf("%d x 1=%d%n", a , a *4 );
+        System.out.printf("%d x 1=%d%n", a,a*5 );
+        System.out.printf("%d x 1=%d%n", a, a *6 );
+        System.out.printf("%d x 1=%d%n", a , a*7 );
+        System.out.printf("%d x 1=%d%n", a ,  a *8 );
+        System.out.printf("%d x 1=%d%n", a , a *9 );
+        System.out.printf("%d x 1=%d%n", a , a*10);
+
+    }
+}
