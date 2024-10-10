58464947b514428c5d3d4d0ad8f37375ff3cb774
shaiksharif <shaikmastansharif116@gmail.com>
2024-10-10 07:46:15 +0530

    string palindrome

---
+++ src/palidromeString.java
@@ -0,0 +1,15 @@
+public class palidromeString {
+    public static boolean ispalidrome(String str){
+        String rev = new StringBuilder(str).reverse().toString();
+        return str.equals(rev);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ispalidrome("madam"));//true
+        System.out.println(ispalidrome("sir"));//false
+    }
+}
+
+
+
+
