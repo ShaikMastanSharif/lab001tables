3f79f3a7e7216c94a7eb7897793d394b35b7a4f7
shaiksharif <shaikmastansharif116@gmail.com>
2024-10-13 06:45:33 +0530

    charactersduplicate

---
+++ src/LengthofString.java
@@ -0,0 +1,19 @@
+public class LengthofString {
+    public static void main(String[] args) {
+        String str = "mastAn sharif";
+        int count = 0;
+
+        for (char c : str.toCharArray()) {
+            count++;
+        }
+        System.out.println(count);
+
+
+       }
+
+
+    }
+
+
+
+
