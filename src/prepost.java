32b44660c327c2760bc3bc7ababff6dd498416ef
shaiksharif <shaikmastansharif116@gmail.com>
2024-10-15 08:35:37 +0530

    prepostincreament

---
+++ src/prepost.java
@@ -0,0 +1,18 @@
+public class prepost {
+    public static void main(String[] args) {
+    int a=60;
+    int b =--a + --a + ++a + a++ - a-- + --a - ++a;
+        System.out.println(b);
+
+
+        int  a1=70;
+        int b1= --a + ++a - ++a - ++a - a-- + ++a;
+        System.out.println(b1);
+
+
+
+    }
+
+
+
+}
