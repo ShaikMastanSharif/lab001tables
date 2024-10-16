a8ebbe3b8f97f04013dd9ff7c027473ec99988f8
shaiksharif <shaikmastansharif116@gmail.com>
2024-10-16 07:16:20 +0530

    nestedif

---
+++ src/condition.java
@@ -0,0 +1,7 @@
+public class condition {
+    public static void main(String[] args) {
+   int age =25;
+   int income=500;
+        System.out.println(age>=18 && income>=500 ? "your are eigible for the loan":"your are not eligible for the loan" );
+    }
+}
